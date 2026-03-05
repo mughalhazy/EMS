bcf0b428bcc70b02c6617576843bb7e4ca440cc8
mughalhazy <mughalhazy@gmail.com>
2026-03-05 17:22:24 +0500

    Add registration REST CRUD-style endpoints

--- ems/services/registration/src/registration.module.ts
+++ ems/services/registration/src/registration.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { EventSettingEntity } from '../../event/src/entities/event-setting.entity';
 import { RegistrationEntity } from './entities/registration.entity';
+import { RegistrationController } from './registration.controller';
 import { RegistrationService } from './registration.service';
 import { RegistrationEventsPublisher } from './registration-events.publisher';
 
 @Module({
   imports: [TypeOrmModule.forFeature([RegistrationEntity, EventSettingEntity])],
+  controllers: [RegistrationController],
   providers: [RegistrationService, RegistrationEventsPublisher],
   exports: [RegistrationService],
 })
