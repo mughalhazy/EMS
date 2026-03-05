8c47ea40b0c0b6dea7a163883584ffc74f78af6a
mughalhazy <mughalhazy@gmail.com>
2026-03-05 13:30:09 +0500

    Add audit logging service for auth role and tenant changes

---
+++ ems/services/audit/src/audit.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuditService } from './audit.service';
+import { AuditLogEntity } from './entities/audit-log.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([AuditLogEntity])],
+  providers: [AuditService],
+  exports: [AuditService],
+})
+export class AuditModule {}
