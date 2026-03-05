c1d6a42b6ec4db6b18bf102cedcc1845a24dfaee
mughalhazy <mughalhazy@gmail.com>
2026-03-05 16:09:01 +0500

    Add event settings module for timezone capacity visibility

--- ems/services/event/src/event.module.ts
+++ ems/services/event/src/event.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { EventEntity } from './entities/event.entity';
+import { EventSettingEntity } from './entities/event-setting.entity';
 import { RoomEntity } from './entities/room.entity';
 import { VenueEntity } from './entities/venue.entity';
+import { EventController } from './event.controller';
+import { EventSettingController } from './event-setting.controller';
+import { EventSettingService } from './event-setting.service';
 import { RoomController } from './room.controller';
 import { RoomService } from './room.service';
 import { VenueController } from './venue.controller';
@@ -11,9 +15,9 @@ import { VenueService } from './venue.service';
 import { EventService } from './event.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EventEntity, VenueEntity, RoomEntity])],
-  controllers: [VenueController, RoomController],
-  providers: [EventService, VenueService, RoomService],
-  exports: [EventService, VenueService, RoomService],
+  imports: [TypeOrmModule.forFeature([EventEntity, EventSettingEntity, VenueEntity, RoomEntity])],
+  controllers: [EventController, EventSettingController, VenueController, RoomController],
+  providers: [EventService, EventSettingService, VenueService, RoomService],
+  exports: [EventService, EventSettingService, VenueService, RoomService],
 })
 export class EventModule {}
