b149d5f8b78ba6f015716b765eb72c7c2027fe8a
mughalhazy <mughalhazy@gmail.com>
2026-03-05 16:15:56 +0500

    Emit Kafka lifecycle events for event changes

--- ems/services/event/src/event.module.ts
+++ ems/services/event/src/event.module.ts
@@ -2,25 +2,38 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { EventController } from './event.controller';
-import { EventEntity } from './entities/event.entity';
-import { EventSettingEntity } from './entities/event-setting.entity';
-import { RoomEntity } from './entities/room.entity';
-import { VenueEntity } from './entities/venue.entity';
-import { EventController } from './event.controller';
+import { EventLifecyclePublisher } from './event-lifecycle.publisher';
+import { EventSearchIndexService } from './event-search-index.service';
 import { EventSettingController } from './event-setting.controller';
 import { EventSettingService } from './event-setting.service';
 import { RoomController } from './room.controller';
 import { RoomService } from './room.service';
-import { EventSearchIndexService } from './event-search-index.service';
 import { VenueController } from './venue.controller';
-import { EventController } from './event.controller';
 import { VenueService } from './venue.service';
+import { EventEntity } from './entities/event.entity';
+import { EventSettingEntity } from './entities/event-setting.entity';
+import { RoomEntity } from './entities/room.entity';
+import { VenueEntity } from './entities/venue.entity';
 import { EventService } from './event.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([EventEntity, EventSettingEntity, VenueEntity, RoomEntity])],
   controllers: [EventController, EventSettingController, VenueController, RoomController],
-  providers: [EventService, EventSettingService, VenueService, RoomService],
-  exports: [EventService, EventSettingService, VenueService, RoomService],
+  providers: [
+    EventService,
+    EventSettingService,
+    VenueService,
+    RoomService,
+    EventSearchIndexService,
+    EventLifecyclePublisher,
+  ],
+  exports: [
+    EventService,
+    EventSettingService,
+    VenueService,
+    RoomService,
+    EventSearchIndexService,
+    EventLifecyclePublisher,
+  ],
 })
 export class EventModule {}
