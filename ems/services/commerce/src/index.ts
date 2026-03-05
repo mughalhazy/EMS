a8603844968e7188d969e5e52fce89f612110500
mughalhazy <mughalhazy@gmail.com>
2026-03-05 16:32:39 +0500

    Add commerce order entity, migration, and service

---
+++ ems/services/commerce/src/index.ts
@@ -0,0 +1,4 @@
+export * from './entities/order.entity';
+export * from './migrations/1714000000000-CreateOrdersTable';
+export * from './order.module';
+export * from './order.service';
---
+++ ems/services/commerce/src/order.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { OrderEntity } from './entities/order.entity';
+import { OrderService } from './order.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([OrderEntity])],
+  providers: [OrderService],
+  exports: [OrderService],
+})
+export class OrderModule {}
