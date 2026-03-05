07f33467facf2c4151770896550a19a9f5c0ac89
mughalhazy <mughalhazy@gmail.com>
2026-03-05 13:38:23 +0500

    Add RBAC role and permission management APIs

--- ems/services/auth/src/auth.module.ts
+++ ems/services/auth/src/auth.module.ts
@@ -2,11 +2,17 @@ import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/c
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthService } from './auth.service';
+import { TenantIsolationMiddleware } from './middleware/tenant-isolation.middleware';
+import { RbacService } from './rbac.service';
 import { AuthCredentialEntity } from './entities/auth-credential.entity';
 import { AuthFederatedIdentityEntity } from './entities/auth-federated-identity.entity';
 import { AuthSsoProviderEntity } from './entities/auth-sso-provider.entity';
 import { AuthTokenEntity } from './entities/auth-token.entity';
 import { AuthUserStateEntity } from './entities/auth-user-state.entity';
+import { PermissionEntity } from './entities/permission.entity';
+import { RoleEntity } from './entities/role.entity';
+import { RolePermissionEntity } from './entities/role-permission.entity';
+import { UserRoleAssignmentEntity } from './entities/user-role-assignment.entity';
 import { UserEntity } from '../../user/src/entities/user.entity';
 
 @Module({
@@ -18,10 +24,14 @@ import { UserEntity } from '../../user/src/entities/user.entity';
       AuthSsoProviderEntity,
       AuthFederatedIdentityEntity,
       UserEntity,
+      RoleEntity,
+      PermissionEntity,
+      RolePermissionEntity,
+      UserRoleAssignmentEntity,
     ]),
   ],
-  providers: [AuthService, TenantIsolationMiddleware],
-  exports: [AuthService],
+  providers: [AuthService, RbacService, TenantIsolationMiddleware],
+  exports: [AuthService, RbacService],
 })
 export class AuthModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
--- ems/services/auth/src/index.ts
+++ ems/services/auth/src/index.ts
@@ -8,3 +8,9 @@ export * from './entities/auth-user-state.entity';
 
 export * from './tenant-context';
 export * from './middleware/tenant-isolation.middleware';
+
+export * from './rbac.service';
+export * from './entities/permission.entity';
+export * from './entities/role.entity';
+export * from './entities/role-permission.entity';
+export * from './entities/user-role-assignment.entity';
