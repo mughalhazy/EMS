7631e02be7f313af206bb9e441fa098071921ea9
mughalhazy <mughalhazy@gmail.com>
2026-03-06 11:09:03 +0500

    Add onsite session scan check-in access validation

--- ems/services/onsite/src/onsite.module.ts
+++ ems/services/onsite/src/onsite.module.ts
@@ -1,15 +1,29 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AttendeeScheduleEntity } from '../../agenda/src/entities/attendee-schedule.entity';
+import { SessionEntity } from '../../agenda/src/entities/session.entity';
 import { AttendeeEntity } from '../../attendee/src/entities/attendee.entity';
 import { BadgePrintingService } from './badge-printing.service';
 import { BadgeEntity } from './entities/badge.entity';
 import { CheckInEntity } from './entities/check-in.entity';
+import { ScanningDeviceEntity } from './entities/scanning-device.entity';
+import { SessionCheckInEntity } from './entities/session-check-in.entity';
 import { OnsiteController } from './onsite.controller';
 import { OnsiteService } from './onsite.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AttendeeEntity, BadgeEntity, CheckInEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      AttendeeEntity,
+      AttendeeScheduleEntity,
+      BadgeEntity,
+      CheckInEntity,
+      ScanningDeviceEntity,
+      SessionCheckInEntity,
+      SessionEntity,
+    ]),
+  ],
   controllers: [OnsiteController],
   providers: [OnsiteService, BadgePrintingService],
   exports: [OnsiteService, BadgePrintingService],
