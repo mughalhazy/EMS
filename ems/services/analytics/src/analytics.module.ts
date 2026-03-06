5a40c8da25919c310eeb20132be98c571f7438b6
mughalhazy <mughalhazy@gmail.com>
2026-03-06 11:58:37 +0500

    Add attendee engagement metrics to event analytics

--- ems/services/analytics/src/analytics.module.ts
+++ ems/services/analytics/src/analytics.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { BoothEntity } from '../../exhibitor/src/entities/booth.entity';
+import { AttendeeConnectionEntity } from '../../networking/src/entities/attendee-connection.entity';
+import { CheckInEntity } from '../../onsite/src/entities/check-in.entity';
+import { OrderItemEntity } from '../../commerce/src/entities/order-item.entity';
+import { RegistrationEntity } from '../../registration/src/entities/registration.entity';
+import { AnalyticsController } from './analytics.controller';
 import { AnalyticsEventsConsumer } from './analytics-events.consumer';
 import { AnalyticsMetricsService } from './analytics-metrics.service';
+import { AnalyticsService } from './analytics.service';
 import { EventAnalyticsEntity } from './entities/event-analytics.entity';
 import { ExhibitorAnalyticsEntity } from './entities/exhibitor-analytics.entity';
 import { SessionAnalyticsEntity } from './entities/session-analytics.entity';
+import { TicketEntity } from '../../ticketing/src/entities/ticket.entity';
 
 @Module({
   imports: [
@@ -15,9 +22,15 @@ import { SessionAnalyticsEntity } from './entities/session-analytics.entity';
       SessionAnalyticsEntity,
       ExhibitorAnalyticsEntity,
       BoothEntity,
+      RegistrationEntity,
+      OrderItemEntity,
+      TicketEntity,
+      CheckInEntity,
+      AttendeeConnectionEntity,
     ]),
   ],
-  providers: [AnalyticsMetricsService, AnalyticsEventsConsumer],
-  exports: [AnalyticsMetricsService],
+  controllers: [AnalyticsController],
+  providers: [AnalyticsMetricsService, AnalyticsEventsConsumer, AnalyticsService],
+  exports: [AnalyticsMetricsService, AnalyticsService],
 })
 export class AnalyticsModule {}
