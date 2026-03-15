0347e22a7ff595c8a44b7d342f4397f48bcd86fa
mughalhazy <mughalhazy@gmail.com>
2026-03-15 18:59:09 +0500

    feat(pricing): add pricing module and core entities

---
+++ ems/services/pricing/src/entities/pricing-rule.types.ts
@@ -0,0 +1,10 @@
+export enum PriceRuleType {
+  BasePrice = 'base_price',
+  DynamicAdjustment = 'dynamic_adjustment',
+  ScheduleOverride = 'schedule_override',
+}
+
+export enum DiscountType {
+  Percentage = 'percentage',
+  FixedAmount = 'fixed_amount',
+}
